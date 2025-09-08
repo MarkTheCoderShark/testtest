1ddc93917d88ef8b6263f286be613049093538ac
eseospaceteam <hello@eseospace.com>
2025-09-07 20:34:15 -0700

    Fix: Resolve Next.js build error by disabling static generation for studio client component

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -3,11 +3,8 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
-// Generate static params for static export
-// Return empty array since studio is not part of static site
-export async function generateStaticParams() {
-  return []
-}
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export default function StudioPage() {
   return <NextStudio config={config} />
