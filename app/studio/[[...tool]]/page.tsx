4b2433367d5b673fc920fee6901fe696e10595eb
eseospaceteam <hello@eseospace.com>
2025-09-07 20:29:58 -0700

    Fix studio route - Add empty generateStaticParams for static export

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -3,6 +3,12 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
+// Generate static params for static export
+// Return empty array since studio is not part of static site
+export async function generateStaticParams() {
+  return []
+}
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
